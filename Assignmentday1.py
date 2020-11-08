92bbc84ae5c524e3cf7d79875a5a0bf340cdee6f
kavya459 <68588017+kavya459@users.noreply.github.com>
2020-11-08 15:53:40 +0530

    Add files via upload

---
+++ Assignmentday1.py
@@ -0,0 +1,33 @@
+Data science  Assignment day 1
+
+Question 1
+Given following jumbled word OBANWRI guess the correct English word:
+  Answer: RAINBOW
+
+question 2:
+Write a program which prints "LETS UPGRADE"
+Program:
+
+print("\"LETS UPGRADE\"")
+ 
+ Question 3:
+
+cp=input()
+sp=input()
+if(cp==sp):
+    print("Neither")
+elif(cp<sp):
+    print("profit")
+else:
+    print("loss")
+ Explanation
+Here sp means selling price
+          cp means cost price
+
+Question 4
+
+IR=int(input())
+Euro=print(int(IR*80))
+
+Here in the above program IR means Indian currency and Euro means currrency in Euro
+
